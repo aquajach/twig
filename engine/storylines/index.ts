58ac87fdec56a807515392fbffb82a2a4bb69d7f
Atta Ho <atta.ho@lgt.com>
2026-05-11 23:13:54 +0800

    Move news to the correct storyline ordering

--- engine/storylines/index.ts
+++ engine/storylines/index.ts
@@ -1,8 +1,14 @@
-import type { StorylineGraph } from '@/engine/types';
-import { chartViz } from './chartViz';
-import { ebankingLoginBug } from './ebankingLoginBug';
-import { gameStart } from './gameStart';
-import { hiddenCoffeeQuest } from './hiddenCoffeeQuest';
-import { news } from './news';
+import type { StorylineGraph } from "@/engine/types";
+import { chartViz } from "./chartViz";
+import { ebankingLoginBug } from "./ebankingLoginBug";
+import { gameStart } from "./gameStart";
+import { hiddenCoffeeQuest } from "./hiddenCoffeeQuest";
+import { news } from "./news";
 
-export const allStorylines: StorylineGraph[] = [news, gameStart, ebankingLoginBug, chartViz, hiddenCoffeeQuest];
+export const allStorylines: StorylineGraph[] = [
+  gameStart,
+  ebankingLoginBug,
+  chartViz,
+  news,
+  hiddenCoffeeQuest,
+];
